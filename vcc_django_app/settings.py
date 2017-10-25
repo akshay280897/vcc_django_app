ba6da6edcd27bb14c8afb212ed364426a2b3fff2
shahaji <shahajirao24@gmail.com>
2017-10-25 12:02:48 +0530

    Change the default database to POSTGRESQL

--- vcc_django_app/settings.py
+++ vcc_django_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'vcc_django_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogdb',
+        'USER': 'djangouser',
+        'PASSWORD': 'pasword123',
+        'HOST': 'localhost',
+        'PORT': '9999',
     }
 }
 
